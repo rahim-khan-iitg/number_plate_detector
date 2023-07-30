2fd0962be9876ba2a2f46439e239ec050356728b
rahimkhan01 <k.rahim@iitg.ac.in>
2023-07-30 23:05:30 +0530

    some fixes

--- number_plate_detector/views.py
+++ number_plate_detector/views.py
@@ -5,16 +5,16 @@ import pandas as pd
 from PIL import Image,ImageDraw
 import math
 from . import forms
-import easyocr
+# import easyocr
 import numpy as np
-def read_text(arr):
-    reader=easyocr.Reader(['en'])
-    text=reader.readtext(arr,paragraph=True)
-    return text[0][1]
+# def read_text(arr):
+#     reader=easyocr.Reader(['en'])
+#     text=reader.readtext(arr,paragraph=True)
+#     return text[0][1]
 
 def get_bounding_box(df:pd.DataFrame,img:Image):
     draw=ImageDraw.Draw(img)
-    text=list()
+    # text=list()
     if len(df)==0:
         return
     for i in range(len(df)):
@@ -24,11 +24,10 @@ def get_bounding_box(df:pd.DataFrame,img:Image):
         draw.line((xmin,ymin)+(xmin,ymax),fill=(255,0,0),width=2)
         draw.line((xmin,ymax)+(xmax,ymax),fill=(255,0,0),width=2)
         draw.line((xmax,ymin)+(xmax,ymax),fill=(255,0,0),width=2)
-        cropped=img.crop((xmin,ymin,xmax,ymax))
-        c=np.array(cropped)
-        text.append(read_text(c))
-    return text
-
+        # cropped=img.crop((xmin,ymin,xmax,ymax))
+        # c=np.array(cropped)
+        # text.append(read_text(c))
+    # return text
 @csrf_exempt
 def home(request):
     model=torch.hub.load('ultralytics/yolov5','custom',path='media/model/best.pt')
@@ -37,11 +36,12 @@ def home(request):
         img=Image.open(request.FILES['image'])
         result=model(img)
         df=result.pandas().xyxy[0]
-        text=get_bounding_box(df,img)
-        if text==None:
-            text=[]
-        else:
-            text=" ,".join(text)
+        # text=get_bounding_box(df,img)
+        get_bounding_box(df,img)
+        # if text==None:
+        #     text=[]
+        # else:
+        #     text=" ,".join(text)
         img.save("media/images/a.jpg")
-        return render(request,"index.html",{"image":forms.Image,"encoded_image":'media/images/a.jpg',"text":text})
+        return render(request,"index.html",{"image":forms.Image,"encoded_image":'media/images/a.jpg'})
     return render(request,"index.html",{"image":forms.Image})
